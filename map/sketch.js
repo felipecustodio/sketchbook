8ea8063dc7f5140773b024c34e04dd9eed5f413a
Felipe Scrochio Custódio <felipe.crochi@gmail.com>
2018-04-21 18:30:01 -0300

    Connections now leave trails

--- map/sketch.js
+++ map/sketch.js
@@ -1,20 +1,22 @@
+// map
 var mapimg;
 var clat = 0;
 var clon = 0;
 var ww = 1280;
 var hh = 720;
-var zoom = 1;
+var zoom = 1.4;
 
+// dataset
 var countries;
 
-var trail = [];
-var shifts = 0;
-
-var current;
-var ellipse_size = 5;
+// trail
 var i = 1;
+var time = 0;
 var points = [];
 
+var connections = [];
+
+// web mercator
 function mercX(lon) {
   lon = radians(lon);
   var a = (256 / PI) * pow(2, zoom);
@@ -30,8 +32,15 @@ function mercY(lat) {
   return a * c;
 }
 
+function colorAlpha(aColor, alpha) {
+  var c = color(aColor);
+  if (alpha <= 0.05) {
+      alpha = 0.05;
+  }
+  return color('rgba(' +  [red(c), green(c), blue(c), alpha].join(',') + ')');
+}
+
 function preload() {
-  // The clon and clat in this url are edited to be in the correct order.
   mapimg = loadImage('https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' +
     clon + ',' + clat + ',' + zoom + '/' +
     ww + 'x' + hh +
@@ -41,9 +50,10 @@ function preload() {
 
 function setup() {
 	createCanvas(ww, hh);
-	frameRate(5);
-	stroke("#D45D79");
-	fill("#D45D79");
+
+    smooth();
+	frameRate(20);
+
 	translate(width / 2, height / 2);
 	imageMode(CENTER);
 	image(mapimg, 0, 0);
@@ -69,31 +79,53 @@ function setup() {
 
 		var pos = createVector(x,y);
 		append(points,pos);
-
-		ellipse(x, y, 2, 2);
 	}
 }
 
 function draw() {
 	translate(width / 2, height / 2);
-
+    imageMode(CENTER);
 	image(mapimg, 0, 0);
-	if (i >= points.length) {
-		i = 1;
+
+    for (var j = 0; j < points.length; j++) {
+        stroke("#FBF0F0");
+        fill("#FBF0F0");
+        // noFill();
+		ellipse(points[j].x, points[j].y, 4, 4);
 	}
 
-	if (ellipse_size == 5) {
-		current = i;
-	}
-
-
-	stroke("#D45D79");
+    stroke("#D45D79");
 	fill("#D45D79");
-	ellipse(points[i-1].x,points[i-1].y,5);
-	ellipse(points[i].x,points[i].y,5);
+	ellipse(points[i-1].x, points[i-1].y, 5);
+	ellipse(points[i].x, points[i].y, 5);
 
-	strokeWeight(2);
-	line(points[i-1].x,points[i-1].y,points[i].x,points[i].y);
-	i++;
+    // 0 = max
+    // time atual = min
+    strokeWeight(2);
+    var alpha = 1;
+    stroke(colorAlpha('#D45D79', alpha));
+	// line(points[i-1].x,points[i-1].y,points[i].x,points[i].y);
 
+    country1 = countries[i].split(/,/);
+    country2 = countries[i+1].split(/,/);
+    console.log(country1[3] + " -> " + country2[3]);
+
+    var connection = createVector(i-1,i,1);
+    append(connections, connection);
+    console.log(connections.length);
+
+    // draw all connections
+    for (var j = 0; j < connections.length; j++) {
+        connection = connections[j];
+        stroke(colorAlpha('#D45D79', connection.z));
+        connections[j].z -= 0.15;
+        line(points[connection.x].x,points[connection.x].y,points[connection.y].x,points[connection.y].y);
+    }
+
+    i++;
+    time += 1;
+    if (i >= points.length) {
+		i = 1;
+        // points.shift();
+	}
 }
